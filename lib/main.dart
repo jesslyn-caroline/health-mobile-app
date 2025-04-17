0d57c2329e28e25160df263cb197bb3ac27d3e2d
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-04-17 23:57:10 +0700

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:health_mobile_app/providers/todo_list_provider.dart';
 import 'package:health_mobile_app/screens/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TodoListProvider())
+      ],
+      child: MyApp()
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
