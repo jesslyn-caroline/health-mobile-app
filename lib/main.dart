7eee7310c04b4972895b076a7aa5842aafc1790e
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-04-18 20:51:15 +0700

    add userprofileprovider

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:provider/provider.dart';
 
 import 'package:health_mobile_app/providers/todo_list_provider.dart';
 import 'package:health_mobile_app/screens/home.dart';
+import 'package:health_mobile_app/providers/user_profile_provider.dart';
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => TodoListProvider())
+        ChangeNotifierProvider(create: (context) => TodoListProvider()),
+        ChangeNotifierProvider(create: (context) => UserProfileProvider()),
       ],
       child: MyApp()
     ),
