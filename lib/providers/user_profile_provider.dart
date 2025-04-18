d545c3b38d24a0bb0092bd27dca605ca338bc572
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-04-18 23:14:35 +0700

    add methods and variables

--- lib/providers/user_profile_provider.dart
+++ lib/providers/user_profile_provider.dart
@@ -6,20 +6,94 @@ class UserProfileProvider with ChangeNotifier{
   String password = "cinnamon123";
   String email = "cinnamon@mail.com";
 
-  bool isPassword = true;
+  String invalidMsgUsername = "";
+  String invalidMsgCurrentPassword = "";
+  String invalidMsgNewPassword = "";
+  String invalidMsgConfirmNewPassword = "";
 
   TextEditingController newusernameC = TextEditingController();
-
-  TextEditingController emailC = TextEditingController();
 
   TextEditingController currentpasswordC = TextEditingController();
   TextEditingController newpasswordC = TextEditingController();
   TextEditingController confirmnewpasswordC = TextEditingController();
 
-  void confirmCurrentPassword () {
-    if (currentpasswordC.text != password) isPassword = false;
-    else isPassword = true;
+  void checkUsernameValidity() {
+    List <String> invalidCharacter = ["!", "@", "#", "\$", "%", "^", "&", "*", "+", "=", "|", "\\", ":", ";", ","];
+
+    bool containInvalidCharacter = false;
+
+    for (int i = 0; i < invalidCharacter.length; i++) {
+      if (newusernameC.text.contains(invalidCharacter[i])) {
+        containInvalidCharacter = true;
+      }
+    }
+
+    if (username == newusernameC.text) invalidMsgUsername = "Please enter new username";
+    else if (containInvalidCharacter) invalidMsgUsername = "Username may not contain special character";
+    else invalidMsgUsername = "";
 
     notifyListeners();
+  }
+
+  void confirmCurrentPassword () {
+    if (currentpasswordC.text != password) invalidMsgCurrentPassword = "Password not match";
+    else invalidMsgCurrentPassword = "";
+
+    notifyListeners();
+  }
+
+  bool changeUsername() {
+    if (invalidMsgUsername == "" && invalidMsgCurrentPassword == "") {
+      username = newusernameC.text;
+      clearController();
+      resetinvalidMsg();
+
+      notifyListeners();
+      
+      return true;
+    }
+
+    return false;
+  }
+  
+  void checkNewPassword () {
+    if (newpasswordC.text.isEmpty) invalidMsgNewPassword = "New password may not be empty";
+    else {
+      invalidMsgNewPassword = "";
+      if (newpasswordC.text != confirmnewpasswordC.text) invalidMsgConfirmNewPassword = "New password not match";
+      else invalidMsgConfirmNewPassword = "";
+    }
+
+    notifyListeners();
+  }
+
+  bool changePassword () {
+    if (invalidMsgCurrentPassword == "" && invalidMsgConfirmNewPassword == "" && !newpasswordC.text.isEmpty) {
+      password = newpasswordC.text;
+      clearController();
+      resetinvalidMsg();
+
+      notifyListeners();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  void clearController () {
+    newusernameC.clear();
+    currentpasswordC.clear();
+    newpasswordC.clear();
+    confirmnewpasswordC.clear();
+
+    notifyListeners();
+  }
+
+  void resetinvalidMsg() {
+    invalidMsgUsername = "";
+    invalidMsgCurrentPassword = "";
+    invalidMsgNewPassword = "";
+    invalidMsgConfirmNewPassword = "";
   }
 }
