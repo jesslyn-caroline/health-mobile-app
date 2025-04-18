886d74c78ec5dae51af0b22b523ef5bb7c59f06f
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-04-18 12:49:25 +0700

    new component

---
+++ lib/components/todo_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+class TodoCard extends StatelessWidget {
+  TodoCard({super.key, required this.task, required this.func});
+
+  dynamic task;
+  void Function(dynamic task, bool? value) func;
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      
+      tileColor: Colors.white,
+      title: Text(task["task"], style: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w500)),
+      subtitle: Text(task["category"], style: 
+        GoogleFonts.poppins(
+          fontSize: 13, 
+          fontWeight: FontWeight.w500,
+          color: (
+            task["category"] == "Fitness" ? Color(0xff0369A1) :
+            task["category"] == "Lifestyle" ? Color(0xff16A34A) :
+            task["category"] == "Self Care" ? Color(0xffCA8A04) :
+            Colors.blueGrey
+          )
+        )
+      ),
+      value: task["finished"], 
+      onChanged: (value) {
+        func(task, value);
+      }
+    );
+  }
+}
