5e462f2b68258e28c0992c2a139b713bbf51a629
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-04-18 13:13:08 +0700

    fix layout

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:health_mobile_app/components/new_task.dart';
-import 'package:health_mobile_app/components/todo_card.dart';
+
 import 'package:provider/provider.dart';
 
-import 'package:google_fonts/google_fonts.dart';
-
 import 'package:health_mobile_app/providers/todo_list_provider.dart';
+import 'package:health_mobile_app/components/todo_card.dart';
 
 class TodoList extends StatefulWidget {
   const TodoList({super.key});
@@ -20,29 +18,18 @@ class _TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          (context.watch<TodoListProvider>().currentActive == "todo"? 
-          NewTask() : 
-          Container(width: double.infinity)),   
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                ...context.watch<TodoListProvider>().todoList.where((e) {
-                  return e["finished"] == (context.watch<TodoListProvider>().currentActive == "todo" ? false : true);
-                }).map((task) {
-                  return TodoCard(
-                    task: task, 
-                    func: (task, value) {
-                      context.read<TodoListProvider>().finishTask(task, value);
-                    }
-                  );
-                })
-              ],
-            )
-          ),
-        ]
+      children: [
+        ...context.watch<TodoListProvider>().todoList.where((e) {
+          return e["finished"] == (context.watch<TodoListProvider>().currentActive == "todo" ? false : true);
+        }).map((task) {
+          return TodoCard(
+            task: task, 
+            func: (task, value) {
+              context.read<TodoListProvider>().finishTask(task, value);
+            }
+          );
+        })
+      ]
     );
   }
 }
